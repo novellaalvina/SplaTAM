d7fe6c55c685adbe49374045e3743811f0e077d9
novellaalvina <novella.alvina@gmail.com>
2024-09-13 10:58:56 -0600

    changing the splatam config benchmark for realsense from replica to iphone

--- configs/realsense/splatam.py
+++ configs/realsense/splatam.py
@@ -1,39 +1,53 @@
 import os
 from os.path import join as p_join
 
-scenes = ["attempt1"]
-
-primary_device="cuda:0"
+primary_device = "cuda:0"
 seed = 0
-scene_name = scenes[0]
 
-map_every = 1
-keyframe_every = 5
-mapping_window_size = 24
-tracking_iters = 40
-mapping_iters = 60
+base_dir = "./experiments/realsense" # Root Directory to Save iPhone Dataset
+scene_name = "attempt2" # Scan Name
+num_frames = 100 # Desired number of frames to capture
+depth_scale = 10.0 # Depth Scale used when saving depth
+overwrite = False # Rewrite over dataset if it exists
+
+full_res_width = 680
+full_res_height = 480
+downscale_factor = 1.0
+densify_downscale_factor = 4.0
 
 group_name = "realsense"
 run_name = f"{scene_name}_{seed}"
 
+map_every = 1
+if num_frames < 25:
+    keyframe_every = int(num_frames//5)
+else:
+    keyframe_every = 5
+mapping_window_size = 32
+tracking_iters = 60
+mapping_iters = 60
+
 config = dict(
-    workdir=f"./experiments/{group_name}",
-    run_name=run_name,
+    workdir=f"./{base_dir}/{scene_name}",
+    run_name="SplaTAM_realsense",
+    overwrite=overwrite,
+    depth_scale=depth_scale,
+    num_frames=num_frames,
     seed=seed,
     primary_device=primary_device,
     map_every=map_every, # Mapping every nth frame
     keyframe_every=keyframe_every, # Keyframe every nth frame
     mapping_window_size=mapping_window_size, # Mapping window size
-    report_global_progress_every=500, # Report Global Progress every nth frame
-    eval_every=5, # Evaluate every nth frame (at end of SLAM)
+    report_global_progress_every=100, # Report Global Progress every nth frame
+    eval_every=1, # Evaluate every nth frame (at end of SLAM)
     scene_radius_depth_ratio=3, # Max First Frame Depth to Scene Radius Ratio (For Pruning/Densification)
     mean_sq_dist_method="projective", # ["projective", "knn"] (Type of Mean Squared Distance Calculation for Scale of Gaussians)
     gaussian_distribution="isotropic", # ["isotropic", "anisotropic"] (Isotropic -> Spherical Covariance, Anisotropic -> Ellipsoidal Covariance)
     report_iter_progress=False,
     load_checkpoint=False,
-    checkpoint_time_idx=0,
+    checkpoint_time_idx=130,
     save_checkpoints=False, # Save Checkpoints
-    checkpoint_interval=100, # Checkpoint Interval
+    checkpoint_interval=5, # Checkpoint Interval
     use_wandb=True,
     wandb=dict(
         entity="u1413401",
@@ -44,11 +58,14 @@ config = dict(
         eval_save_qual=True,
     ),
     data=dict(
-        basedir="./data/realsense",
+        dataset_name="realsense",
+        basedir='./data/realsense',
         gradslam_data_cfg="./configs/data/realsense.yaml",
         sequence=scene_name,
-        desired_image_height=480,
-        desired_image_width=640,
+        desired_image_height=int(full_res_height//downscale_factor),
+        desired_image_width=int(full_res_width//downscale_factor),
+        densification_image_height=int(full_res_height//densify_downscale_factor),
+        densification_image_width=int(full_res_width//densify_downscale_factor),
         start=0,
         end=-1,
         stride=1,
@@ -57,11 +74,17 @@ config = dict(
     tracking=dict(
         use_gt_poses=False, # Use GT Poses for Tracking
         forward_prop=True, # Forward Propagate Poses
+        visualize_tracking_loss=True, # Visualize Tracking Diff Images
         num_iters=tracking_iters,
         use_sil_for_loss=True,
         sil_thres=0.99,
         use_l1=True,
+        use_depth_loss_thres=True,
+        depth_loss_thres=20000, # Num of Tracking Iters becomes twice if this value is not met
         ignore_outlier_depth_loss=False,
+        use_uncertainty_for_loss_mask=False,
+        use_uncertainty_for_loss=False,
+        use_chamfer=False,
         loss_weights=dict(
             im=0.5,
             depth=1.0,
@@ -72,8 +95,8 @@ config = dict(
             unnorm_rotations=0.0,
             logit_opacities=0.0,
             log_scales=0.0,
-            cam_unnorm_rots=0.0004,
-            cam_trans=0.002,
+            cam_unnorm_rots=0.001,
+            cam_trans=0.004,
         ),
     ),
     mapping=dict(
@@ -81,8 +104,11 @@ config = dict(
         add_new_gaussians=True,
         sil_thres=0.5, # For Addition of new Gaussians
         use_l1=True,
-        use_sil_for_loss=False,
         ignore_outlier_depth_loss=False,
+        use_sil_for_loss=False,
+        use_uncertainty_for_loss_mask=False,
+        use_uncertainty_for_loss=False,
+        use_chamfer=False,
         loss_weights=dict(
             im=0.5,
             depth=1.0,
@@ -129,6 +155,6 @@ config = dict(
         viz_near=0.01, viz_far=100.0,
         view_scale=2,
         viz_fps=5, # FPS for Online Recon Viz
-        enter_interactive_post_online=True, # Enter Interactive Mode after Online Recon Viz
+        enter_interactive_post_online=False, # Enter Interactive Mode after Online Recon Viz
     ),
 )
